71e3a5c1a807a15aab86d805de39c0ff510bcef9
Szymon Rutkowski <Szymon.rutkowski1@gmail.com>
2025-11-17 15:25:20 +0100

    reorganizing entire js by grouping code into functions

--- js/script.js
+++ js/script.js
@@ -1,20 +1,9 @@
 {
-    const plnToEur = 0.2366;
-    const plnToUsd = 0.2748;
-    const usdToEur = 0.8609;
+    const calculateRate = (currencyChange) => {
+        const plnToEur = 0.2366;
+        const plnToUsd = 0.2748;
+        const usdToEur = 0.8609;
 
-    const formElement = document.querySelector(".js-form");
-
-    formElement.addEventListener("submit", (event) => {
-        event.preventDefault();
-        
-        const firstCurrency = document.querySelector(".js-firstCurrency");
-        const secondCurrency = document.querySelector(".js-secondCurrency");
-        const amountElement = document.querySelector(".js-amount");
-        const resultElement = document.querySelector(".js-result");
-        const indexElement = document.querySelector(".js-index");
-
-        const currencyChange = `${firstCurrency.value}/${secondCurrency.value}`;
         let rate;
 
         switch (currencyChange) {
@@ -40,9 +29,35 @@
                 rate = 1;
         }
 
-        const result = parseFloat(amountElement.value) * parseFloat(rate);
+        return rate;
+    };
+
+    const updateResultText = (result, secondCurrency, amountElement, firstCurrency) => {
+        const resultElement = document.querySelector(".js-result");
+        const indexElement = document.querySelector(".js-index");
 
         indexElement.innerText = `${parseFloat(amountElement.value)} ${firstCurrency.value} = `;
         resultElement.innerText = `${result.toFixed(2)} ${secondCurrency.value}`;
-    });
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const firstCurrency = document.querySelector(".js-firstCurrency");
+        const secondCurrency = document.querySelector(".js-secondCurrency");
+        const amountElement = document.querySelector(".js-amount");
+
+        const currencyChange = `${firstCurrency.value}/${secondCurrency.value}`;
+        const rate = calculateRate(currencyChange);
+        const result = parseFloat(amountElement.value) * rate;
+
+        updateResultText(result, secondCurrency, amountElement, firstCurrency);
+    };
+
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+        formElement.addEventListener("submit", onFormSubmit);
+    };
+
+    init();
 }
